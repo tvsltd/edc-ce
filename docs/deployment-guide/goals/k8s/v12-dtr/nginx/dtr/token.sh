a13126de94fb1250340b9f0542983b8e9fb6f88a
umarfchy <umarfchy@gmail.com>
2025-05-21 08:37:18 +0300

    feat: update Keycloak token script and ingress configuration with new host and improved debugging

--- docs/deployment-guide/goals/k8s/v12-dtr/nginx/dtr/token.sh
+++ docs/deployment-guide/goals/k8s/v12-dtr/nginx/dtr/token.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Keycloak configuration
-KEYCLOAK_URL="http://localhost:8080"
+# Keycloak configuration based on default-realm-import.json
+# Realm: default-realm
+# Client: default-client (service account enabled)
+# Token lifespan: 300 seconds (5 minutes)
+KEYCLOAK_URL="https://kc-dtr-int-sovity.tvsdevops.co.uk"
 REALM="default-realm"
 TOKEN_ENDPOINT="${KEYCLOAK_URL}/realms/${REALM}/protocol/openid-connect/token"
 
-# Client credentials
+# Client credentials from default-realm-import.json
 CLIENT_ID="default-client"
 CLIENT_SECRET="wJcfhf5uXynRcAHy5Ua9KAwM4EhsFvC1"
 
-curl -X POST \
+# Add verbose output for debugging
+echo "Requesting token from: ${TOKEN_ENDPOINT}"
+
+# Make the request
+curl -v -X POST \
   "${TOKEN_ENDPOINT}" \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   --data-urlencode 'grant_type=client_credentials' \
   --data-urlencode "client_id=${CLIENT_ID}" \
   --data-urlencode "client_secret=${CLIENT_SECRET}"
+
+# Add newline for better output formatting
+echo
